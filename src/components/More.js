cd50e8c657eadd3267d2af9c82ff14de2192ae9b
David Liu <git-commit-address>
2021-05-07 15:03:16 -0400

    delete donate

--- src/components/More.js
+++ src/components/More.js
@@ -8,7 +8,6 @@ function More() {
             <Header />
             <a href="/ibdreddit" className="navbar-item">IBD Forum</a>
             <a href="/ibdaiddiet" className="navbar-item">IBD AID Diet</a>
-            <a href="/donate" className="navbar-item">Donate</a>
 
         </div>
     )
